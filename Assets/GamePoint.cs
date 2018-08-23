5a74d2952a5f85233ff6b4fb8e16cd6231a6685b
tatsunari.kataoka <ryusei11121991@gmail.com>
2018-08-23 23:17:33 +0900

    Lesson5課題：得点を表示しよう GamePointスクリプト：得点表示・加算・更新 Testスクリプト：衝突判定・衝突物ごとの得点指示・GamePointクラスの取得
    通常だともっと簡単にできるらしいですが、一旦勉強の意味も含めて思いついた方法で実装。

---
+++ Assets/GamePoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePoint : MonoBehaviour
+{
+	GameObject GamePointText;
+
+	public int Point = 0;        //ポイントの定義と初期化
+
+	//ポイントの加算関数
+	public int PointAdd(int a)
+	{
+		Point += a;
+		Debug.Log( Point );
+		this.GamePointText.GetComponent<Text>().text = "POINT:" + Point ;
+		return Point;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		//シーン中のGamePointオブジェクトを取得
+		this.GamePointText = GameObject.Find("GamePointText");
+		//GamePointオブジェクトにポイント0を表示
+		this.GamePointText.GetComponent<Text>().text = "POINT:" + Point ;
+		Debug.Log( Point );
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
+	
---
+++ Assets/Test.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour 
+{
+	private GamePoint gamePoint;
+
+	// Use this for initialization
+	void Start()
+	{
+		gamePoint = GameObject.Find("Ball").GetComponent<GamePoint>();
+		//シーン中のGamePointオブジェクトを取得
+	}
+	void Update()
+	{
+	}
+
+	//衝突判定
+	//衝突物の特定
+	//衝突物に応じたポイントの指定
+	//指定したポイントをカウンター関数に引数として与える。
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision.collider.name);
+
+		//ボールがLargeCloudに衝突した場合
+		if (collision.gameObject.CompareTag("LargeCloudTag"))
+		{
+			//ポイント10を引数としてカウンター関数に渡す
+			//GamePointクラスの変数を宣言してインスタンスを代入
+			gamePoint.PointAdd(10);
+			//ポイント加算用の関数を呼び出す
+
+			//ボールがSmallCloudに衝突した場合
+		}
+		else if (collision.gameObject.CompareTag("SmallCloudTag"))
+		{
+			//ポイント20を引数としてカウンター関数に渡す
+			//GamePointクラスの変数を宣言してインスタンスを代入
+			gamePoint.PointAdd(20);
+			//ポイント加算用の関数を呼び出す
+			Debug.Log(collision.collider.name);
+
+			//ボールがLargeStarに衝突した場合
+		}
+		else if (collision.gameObject.CompareTag("LargeStarTag"))
+		{
+			//ポイント30を引数としてカウンター関数に渡す
+			//GamePointクラスの変数を宣言してインスタンスを代入
+			gamePoint.PointAdd(30);
+			//ポイント加算用の関数を呼び出す
+			Debug.Log(collision.collider.name);
+		
+			//ボールがSmallStarに衝突した場合
+		}
+		else if (collision.gameObject.CompareTag("SmallStarTag"))
+		{
+			//ポイント40を引数としてカウンター関数に渡す
+			gamePoint.PointAdd(40);
+			//ポイント加算用の関数を呼び出す
+			Debug.Log(collision.collider.name);
+		}
+		//GamePointTextオブジェクトに合計ポイントを表示
+
+	}
+
+}	
